0decbfb09e2dbf96375b283ee1cac5548b48d33c
Roy <love2533728@gmail.com>
2024-09-13 18:55:40 +0800

    Added GET,POST,PUT,PATCH and DELETE method to our APIView

--- api_profile/views.py
+++ api_profile/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from api_profile import serializers
 class HelloApiView(APIView):
+
+    serializer_class=serializers.HelloSerializer
     def get(self,request,format=None):
         """Retutrn a list of APIView features"""
         an_apiview=[
@@ -11,3 +15,28 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs'
         ]
         return Response({'message':'hello!','an_apiview':an_apiview})
+    
+
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f"hello {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+    
+    def patch(self,request,pk=None):
+        """Handle  a partial update of an object"""
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
